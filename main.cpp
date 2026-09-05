885a60b9ab71ab37ea01f75c3aff9b69447f0f33
henry-w-slough <henry.w.slough@gmail.com>
2026-09-05 14:28:01 -0400

    Added MacOS build support

--- main.cpp
+++ main.cpp
@@ -5,14 +5,12 @@
 
 int main(int argc, char* argv[]) {
 
-
     Screen screen = Screen(800, 800, "sdl2 test window", SDL_WINDOW_SHOWN, SDL_RENDERER_ACCELERATED);
     screen.setBackgroundColor(200, 200, 210, 255);
 
     while (screen.isRunning) {
         screen.Update();
     }
-
 
     return 0;
 }
